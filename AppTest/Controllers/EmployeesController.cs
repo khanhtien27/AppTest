56cad47033b585e189dcef5b7adbbef727919244
TienNguyen <tiennguyenwcl@gmail.com>
2023-06-09 12:06:36 -0700

    add EmployeeControler and View

--- AppTest/Controllers/EmployeesController.cs
+++ AppTest/Controllers/EmployeesController.cs
@@ -17,7 +17,7 @@ namespace AppTest.Controllers
         }
         public async Task<IActionResult> Index(int? Id)
         {
-            if(Id == null)
+            if (Id == null)
             {
                 var model = await _context.Users.ToListAsync();
                 return View(model);
@@ -51,7 +51,7 @@ namespace AppTest.Controllers
         public async Task<IActionResult> ChangeAvatar(string Id, string img)
         {
             var model = await _context.Users.FindAsync(Id);
-            if(model == null)
+            if (model == null)
             {
                 return NotFound();
             }
@@ -64,24 +64,44 @@ namespace AppTest.Controllers
                 return RedirectToAction(nameof(Index));
             }
         }
-
-
-        //public async Task<IActionResult> TimeSheetByMonth()
-        //{
-        //    TimeSheetMonth timeSheetMonth = new TimeSheetMonth();
-        //    return View(timeSheetMonth);
-        //}
-
-        public async Task<IActionResult> TimeSheetByMonth (int? Month)
+        public async Task<IActionResult> TimeSheetByMonth(int? Month)
         {
-            if(Month != null)
+            if (Month != null)
             {
-                
-                var model= await _context.TimeSheet.Where(t => t.Start.Month == Month).Include(e => e.employee).AsNoTracking().ToListAsync(); 
+                Dictionary<string, TimeSpan> ToltalTime = new Dictionary<string, TimeSpan>();
+
+                var model = await _context.TimeSheet.Where(t => t.Start.Month == Month).Include(e => e.employee).AsNoTracking().ToListAsync();
                 if (model == null)
                 {
                     return NotFound();
                 }
+
+                for (int i = 0; i < model.Count; i++)
+                {
+                    TimeSpan totalTime = new TimeSpan();
+                    var item = model[i];
+                    if (item.BreakStart == null)
+                    {
+                        totalTime = (TimeSpan)(item.End - item.Start);
+                    }
+                    else
+                    {
+                        totalTime = (TimeSpan)(item.BreakStart - item.Start) + (TimeSpan)(item.End - item.BreakEnd);
+                    }
+
+                    if (ToltalTime.TryAdd(item.IdEmployment, totalTime) == false)
+                    {
+                        var a = ToltalTime.TryGetValue(item.IdEmployment, out var b);
+                        b += totalTime;
+                        ToltalTime[item.IdEmployment] = b;
+                    }
+                }
+                var n = ToltalTime.MaxBy(i => i.Value);
+                var m = n.Key;
+                var time = n.Value;
+                ViewBag.Max = (time.TotalMinutes / 60).ToString("##0.##");
+                var employee = await _context.Users.FindAsync(m);
+                ViewBag.Employee = employee.FirstName + " " + employee.LastName;
                 return View(model);
             }
             else
@@ -93,11 +113,66 @@ namespace AppTest.Controllers
                 }
                 return View(model);
             }
-            
         }
 
+        public async Task<IActionResult> CalculateSalary(int? Month)
+        {
+            if (Month == null)
+            {
+                return View();
+            }
+            const double regularPayment = 3.125;
+            Dictionary<string, TimeSpan> ToltalTime = new Dictionary<string, TimeSpan>();
+            Dictionary<Employee, double> Salary = new Dictionary<Employee, double>();
+            var model = await _context.TimeSheet.Where(t => t.Start.Month == Month).Include(e => e.employee).AsNoTracking().ToListAsync();
+            if (model == null)
+            {
+                return NotFound();
+            }
 
-        
+            for (int i = 0; i < model.Count; i++)
+            {
+                TimeSpan totalTime = new TimeSpan();
+                var item = model[i];
+                if (item.BreakStart == null)
+                {
+                    totalTime = (TimeSpan)(item.End - item.Start);
+                }
+                else
+                {
+                    totalTime = (TimeSpan)(item.BreakStart - item.Start) + (TimeSpan)(item.End - item.BreakEnd);
+                }
 
+                if (ToltalTime.TryAdd(item.IdEmployment, totalTime) == false)
+                {
+                    var a = ToltalTime.TryGetValue(item.IdEmployment, out var b);
+                    b += totalTime;
+                    ToltalTime[item.IdEmployment] = b;
+                }
+            }
+            foreach (var employee in ToltalTime)
+            {
+                if (employee.Value.TotalMinutes / 60 < 40)
+                {
+                    var a = await _context.Users.FindAsync(employee.Key);
+                    var b = (employee.Value.TotalMinutes / 60) * regularPayment;
+                    Salary.Add(a, b);
+                }
+                else
+                {
+                    var a = await _context.Users.FindAsync(employee.Key);
+                    var b = (employee.Value.TotalMinutes / 60) * regularPayment * 1.5;
+                    Salary.Add(a, b);
+                }
+            }
+            return View(Salary);
+        }
+
+        public async Task<IActionResult> Infor (string Id)
+        {
+            var model = await _context.Users.FindAsync(Id);
+            if (model == null) return NotFound();
+            return View(model);
+        }
     }
 }
