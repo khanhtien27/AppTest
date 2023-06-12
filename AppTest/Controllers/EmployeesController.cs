52f583f4605e3c7b74c805a789763e80e7280c43
Khanh Tien <tien.hjhj10@gamil.com>
2023-06-12 20:31:01 +0700

    fix bug

--- AppTest/Controllers/EmployeesController.cs
+++ AppTest/Controllers/EmployeesController.cs
@@ -71,38 +71,44 @@ namespace AppTest.Controllers
                 Dictionary<string, TimeSpan> ToltalTime = new Dictionary<string, TimeSpan>();
 
                 var model = await _context.TimeSheet.Where(t => t.Start.Month == Month).Include(e => e.employee).AsNoTracking().ToListAsync();
-                if (model == null)
+                if (model.Count == 0)
                 {
-                    return NotFound();
+                    return View();
                 }
-
-                for (int i = 0; i < model.Count; i++)
+                else
                 {
-                    TimeSpan totalTime = new TimeSpan();
-                    var item = model[i];
-                    if (item.BreakStart == null)
+                    for (int i = 0; i < model.Count; i++)
                     {
-                        totalTime = (TimeSpan)(item.End - item.Start);
-                    }
-                    else
-                    {
-                        totalTime = (TimeSpan)(item.BreakStart - item.Start) + (TimeSpan)(item.End - item.BreakEnd);
-                    }
+                        TimeSpan totalTime = new TimeSpan();
+                        var item = model[i];
+                        if (item.BreakStart == null)
+                        {
+                            totalTime = (TimeSpan)(item.End - item.Start);
+                        }
+                        else
+                        {
+                            totalTime = (TimeSpan)(item.BreakStart - item.Start) + (TimeSpan)(item.End - item.BreakEnd);
+                        }
 
-                    if (ToltalTime.TryAdd(item.IdEmployment, totalTime) == false)
-                    {
-                        var a = ToltalTime.TryGetValue(item.IdEmployment, out var b);
-                        b += totalTime;
-                        ToltalTime[item.IdEmployment] = b;
+                        if (ToltalTime.TryAdd(item.IdEmployment, totalTime) == false)
+                        {
+                            var a = ToltalTime.TryGetValue(item.IdEmployment, out var b);
+                            b += totalTime;
+                            ToltalTime[item.IdEmployment] = b;
+                        }
                     }
+                    
+                    if(ToltalTime.MaxBy(i => i.Value).Key != null)
+                    {
+                        var n = ToltalTime.MaxBy(i => i.Value);
+                        var m = n.Key;
+                        var time = n.Value;
+                        ViewBag.Max = (time.TotalMinutes / 60).ToString("##0.##");
+                        var employee = await _context.Users.FindAsync(m);
+                        ViewBag.Employee = employee.FirstName + " " + employee.LastName;
+                    }
+                    return View(model);
                 }
-                var n = ToltalTime.MaxBy(i => i.Value);
-                var m = n.Key;
-                var time = n.Value;
-                ViewBag.Max = (time.TotalMinutes / 60).ToString("##0.##");
-                var employee = await _context.Users.FindAsync(m);
-                ViewBag.Employee = employee.FirstName + " " + employee.LastName;
-                return View(model);
             }
             else
             {
@@ -168,9 +174,9 @@ namespace AppTest.Controllers
             return View(Salary);
         }
 
-        public async Task<IActionResult> Infor (string Id)
+        public async Task<IActionResult> Infor(string Id)
         {
-            var model = await _context.Users.FindAsync(Id);
+            var model = await _context.Users.Include(i => i.department).FirstOrDefaultAsync(i => i.Id == Id);
             if (model == null) return NotFound();
             return View(model);
         }
